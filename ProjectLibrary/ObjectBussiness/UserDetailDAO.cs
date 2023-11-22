95436d291889c9d20e64aecf63d2d4fc6f52473f
LoanLe22 <vlyloan@gmail.com>
2023-11-22 15:50:44 +0700

    feat/ApiUserDetail - Viet phan API cho bang UserDetail

---
+++ ProjectLibrary/ObjectBussiness/UserDetailDAO.cs
@@ -0,0 +1,108 @@
+﻿using ProjectLibrary.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.ObjectBussiness
+{
+    public class UserDetailDAO
+    {
+        private static UserDetailDAO instance = null;
+        private static readonly object instanceLock = new object();
+        public static UserDetailDAO Instance
+        {
+            //Singlestone pattern
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new UserDetailDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public List<UserDetail> GetUserDetails()
+        {
+            var listUserDetails = new List<UserDetail>();
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    listUserDetails = context.UserDetails.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return listUserDetails;
+        }
+        public UserDetail FindUserDetailById(int userId)
+        {
+            UserDetail u = new UserDetail();
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    u = context.UserDetails.SingleOrDefault(x => x.UserId == userId);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return u;
+        }
+        public void SaveUserDetail(UserDetail u)
+        {
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    context.UserDetails.Add(u);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public void UpdateUserDetail(UserDetail u)
+        {
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    context.Entry<UserDetail>(u).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public void DeleteUserDetail(UserDetail u)
+        {
+            try
+            {
+                using (var context = new CookingWebsiteContext())
+                {
+                    var u1 = context.UserDetails.SingleOrDefault(n => n.UserId == u.UserId);
+                    context.UserDetails.Remove(u1);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ ProjectLibrary/Repository/UserDetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProjectLibrary.DataAccess;
+using ProjectLibrary.ObjectBussiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.Repository
+{
+    public class UserDetailRepository : IUserDetailRepository
+    {
+        public void DeleteUserDetail(UserDetail u) => UserDetailDAO.Instance.DeleteUserDetail(u);
+        public void SaveUserDetail(UserDetail u) => UserDetailDAO.Instance.SaveUserDetail(u);
+        public void UpdateUserDetail(UserDetail u) => UserDetailDAO.Instance.UpdateUserDetail(u);
+        public List<UserDetail> GetUserDetails() => UserDetailDAO.Instance.GetUserDetails();
+        public UserDetail GetUserDetailById(int id) => UserDetailDAO.Instance.FindUserDetailById(id);
+    }
+}
