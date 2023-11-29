27495497f2817de0d3c822df765f7f44c2671dad
vinhbeoo <letruongvinh0409@gmail.com>
2023-11-30 03:03:55 +0700

    feat/AdminContest: Tạo CRUD cho đối tượng Contest đối với quyền Admin, sử dụng Ajax (Chưa chạy được, pull lên mục đích để hỏi thầy)

---
+++ ProjectWebMVC/Areas/Admin/Controllers/ContestController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectLibrary.ObjectBussiness;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class ContestController : Controller
+{
+    private readonly HttpClient _httpClient;
+    private string ContestUrl = "https://localhost:7269/api/Contest";
+
+    public ContestController()
+    {
+        _httpClient = new HttpClient();
+        var contextType = new MediaTypeWithQualityHeaderValue("application/json");
+        _httpClient.DefaultRequestHeaders.Accept.Add(contextType);
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync(ContestUrl);
+        var strData = await response.Content.ReadAsStringAsync();
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+        List<Contest> contestList = JsonSerializer.Deserialize<List<Contest>>(strData, options);
+        return View(contestList);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync($"{ContestUrl}/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            string strData = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            Contest contest = JsonSerializer.Deserialize<Contest>(strData, options);
+
+            return Json(new { success = true, data = contest });
+        }
+
+        return Json(new { success = false, message = "Contest not found" });
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<JsonResult> Create([FromBody] Contest contest)
+    {
+        if (ModelState.IsValid)
+        {
+            var contestData = JsonSerializer.Serialize(contest);
+            var contentData = new StringContent(contestData, System.Text.Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PostAsync(ContestUrl, contentData);
+            if (response.IsSuccessStatusCode)
+            {
+                return Json(new { success = true, message = "Contest inserted successfully" });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Error while calling Web API" });
+            }
+        }
+        return Json(new { success = false, message = "Invalid model state" });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync($"{ContestUrl}/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            string strData = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            Contest contest = JsonSerializer.Deserialize<Contest>(strData, options);
+            return Json(new { success = true, data = contest });
+        }
+        return Json(new { success = false, message = "Error while calling Web API" });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [FromBody] Contest contest)
+    {
+        if (ModelState.IsValid)
+        {
+            string contestData = JsonSerializer.Serialize(contest);
+            var contentData = new StringContent(contestData, System.Text.Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PutAsync($"{ContestUrl}/{id}", contentData);
+            if (response.IsSuccessStatusCode)
+            {
+                return Json(new { success = true, message = "Contest updated successfully" });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Error while calling Web API" });
+            }
+        }
+        return Json(new { success = false, message = "Invalid model state" });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int id)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync($"{ContestUrl}/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            string strData = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            Contest contest = JsonSerializer.Deserialize<Contest>(strData, options);
+            return Json(new { success = true, data = contest });
+        }
+        return Json(new { success = false, message = "Error while calling Web API" });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id, IFormCollection collection)
+    {
+        HttpResponseMessage response = await _httpClient.DeleteAsync($"{ContestUrl}/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            return Json(new { success = true, message = "Contest deleted successfully" });
+        }
+        return Json(new { success = false, message = "Error while calling Web API" });
+    }
+}
---
+++ ProjectWebMVC/wwwroot/Admin/assets/js/Controllers/contestController.js
@@ -0,0 +1,135 @@
+﻿var contest = {
+    init: function () {
+        contest.registerEvents();
+    },
+    registerEvents: function () {
+        // Detail Contest
+        $(document).on('click', '.detail', function () {
+            var id = $(this).data('id');
+            $.get('/Admin/Contest/Details', { id: id })
+                .done(function (response) {
+                    if (response.success) {
+                        $('#modal-form').modal('show');
+                        $('.modal-title').text('Hiển thị thông tin chi tiết cuộc thi');
+                        // Update fields with contest details
+                        $('#ContestNameDetail').text(response.data.contestName);
+                        $('#DescriptionDetail').text(response.data.description);
+                        $('#StartTimeDetail').text(response.data.startTime);
+                        $('#EndTimeDetail').text(response.data.endTime);
+                        $('#OwnerUserIdDetail').text(response.data.ownerUserId);
+                    } else {
+                        alert(response.message);
+                    }
+                })
+                .fail(function (error) {
+                    console.log(error);
+                });
+        });
+        // Save news contest
+        $(document).on('click', '#btn-save', function () {
+            var form = $('#form-contest');
+            $.post($(this).attr('data-action'), form.serialize())
+                .done(function (response) {
+                    if (response.success) {
+                        location.reload();
+                    } else {
+                        alert(response.message);
+                    }
+                })
+                .fail(function (error) {
+                    console.log(error);
+                });
+        });
+        // Save contest
+        /*$(document).on('click', '#btn-save', function () {
+            var form = $('#form-contest');
+            var contestDetail = {
+                ContestName: $('#ContestName').val(),
+                Description: $('#Description').val(),
+                StartTime: $('#StartTime').val(),
+                EndTime: $('#EndTime').val(),
+                OwnerUserId: $('#OwnerUserId').val()
+            };
+            var data = form.serializeArray();
+            data.push({ name: 'contestDetail.ContestName', value: contestDetail.ContestName });
+            data.push({ name: 'contestDetail.Description', value: contestDetail.Description });
+            data.push({ name: 'contestDetail.StartTime', value: contestDetail.StartTime });
+            data.push({ name: 'contestDetail.EndTime', value: contestDetail.EndTime });
+            data.push({ name: 'contestDetail.OwnerUserId', value: contestDetail.OwnerUserId });
+
+            $.ajax({
+                url: $(this).data('action'),
+                type: 'POST',
+                data: $.param(data),
+                success: function (response) {
+                    if (response.success) {
+                        location.reload();
+                    } else {
+                        alert(response.message);
+                    }
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        });*/
+
+        // Edit contest
+        $(document).on('click', '.edit', function () {
+            var id = $(this).data('id');
+            $.get('/Admin/Contest/Edit', { id: id })
+                .done(function (response) {
+                    if (response.success) {
+                        $('#modal-form').modal('show');
+                        $('.modal-title').text('Chỉnh sửa cuộc thi');
+                        $('#btn-save').attr('data-action', '/Admin/Contest/Edit');
+                        // Update fields with contest details
+                        $('#ContestId').val(response.data.contestId);
+                        $('#ContestName').val(response.data.contestName);
+                        $('#Description').val(response.data.description);
+                        $('#StartTime').val(response.data.startTime);
+                        $('#EndTime').val(response.data.endTime);
+                        $('#OwnerUserId').val(response.data.ownerUserId);
+                    } else {
+                        alert(response.message);
+                    }
+                })
+                .fail(function (error) {
+                    console.log(error);
+                });
+        });
+
+        // Delete contest
+        $(document).on('click', '.delete', function () {
+            var id = $(this).data('id');
+            $('#delete-id').val(id);
+            $('#modal-delete').modal('show');
+        });
+
+        // Delete contest
+        $(document).on('click', '#btn-delete', function () {
+            var id = $('#delete-id').val();
+            $.post('/Admin/Contest/Delete', { contestId: id })
+                .done(function (response) {
+                    if (response.success) {
+                        $('#modal-delete').modal('hide');
+                        location.reload();
+                    } else {
+                        alert(response.message);
+                    }
+                })
+                .fail(function (error) {
+                    console.log(error);
+                });
+        });
+
+        // Create Contest
+        $(document).on('click', '.create', function () {
+            $('#modal-form').modal('show');
+            $('.modal-title').text('Tạo mới cuộc thi');
+            $('#btn-save').attr('data-action', '/Admin/Contest/Create');
+            $('#form-contests').trigger('reset');
+        });
+    }
+};
+contest.init();
